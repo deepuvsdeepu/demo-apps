1fb8394a5b79592f82a55e9a1edba45ea4f77a67
Pradeep <deepuvsdeepu@gmail.com>
2024-09-07 15:37:47 +0100

    Add version to spring boot app

--- spring-boot-sample/src/main/java/com/example/web/controller/VersionController.java
+++ spring-boot-sample/src/main/java/com/example/web/controller/VersionController.java
@@ -1,14 +1,17 @@
 package com.example.web.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class VersionController {
     public record VersionResponse(String version) {}
+    @Value("${spring.application.version}")
+    private String version;
 
     @GetMapping("/version")
     public VersionResponse version() {
-        return new VersionResponse("v2");
+        return new VersionResponse(version);
     }
 }
